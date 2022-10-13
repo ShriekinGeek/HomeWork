c018c2f99ecc8bf6773d101233137140b5cc820d
Голубев Егор <egorororo@outlook.com>
2022-10-13 16:23:27 +0300

    Инициировал работу с Git

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+Console.Write("Input a first number: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a second number: ");
+int n2 = Convert.ToInt32(Console.ReadLine());
+
+int quad2 = n2 * n2;
+
+if(n1 == quad2)
+    Console.WriteLine("Yes");
+else
+    Console.WriteLine("No");
+*/
+
+/*
+Console.Write("Input a positive integer number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int current = -n;
+
+while(current <= n)
+{
+    Console.Write(current + " ");
+    current++;
+}
+*/
+
+/*
+Console.Write("Input a three-digit number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int result = num % 10;
+Console.WriteLine($"Last digit of {num} is {result}");
+*/
+
